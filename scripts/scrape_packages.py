75e0709f5661aa1520d6690d06ce2337ac51b600
Aaron Fraint <aaronfraint@icloud.com>
2021-12-06 08:45:16 -0500

    begin breaking script into single-purpose functions

--- scripts/scrape_packages.py
+++ scripts/scrape_packages.py
@@ -2,6 +2,7 @@
 # TO DO:
 # need a way to flag and fix multipiece segments
 # delete bottom row - totals
+from __future__ import annotations
 from pathlib import Path
 import tabula
 import os
@@ -21,67 +22,88 @@ def Insert_row(row_number, df, row_values):
     return df_result
 
 
-for filepath in Path(pdf_folderpath).rglob("*.pdf"):
-
-    # determine number of pages in PDF
+def get_number_of_pages_in_pdf(filepath: Path | str) -> int:
+    """
+    Open a PDF file and return the number of pages within
+    """
     with open(filepath, "rb") as pdf_file:
         pdf_reader = PdfFileReader(pdf_file)
-        num_pgs = pdf_reader.numPages
+        return pdf_reader.numPages
 
-    # append records from each page to dataframe
-    frames = []
-    for i in range(1, num_pgs + 1):
-        table = tabula.read_pdf(filepath, pages=i)
-        table = table[0]
-        # rename columne
-        table.columns = ["project_num", "SR_name", "from", "to", "scope", "miles", "muni", "adt"]
-        # drop rows 1 & 2 and column 1 (with assigned row numbers)
-        table = table.iloc[2:, :]
 
-        # parse out segment/offset from 'from' and 'to' columns for mapping
-        table[["fsegment", "from"]] = table["from"].str.split("\r", n=1, expand=True)
-        table[["fsegment", "foffset"]] = table["fsegment"].str.split("/", n=1, expand=True)
-        table[["tsegment", "to"]] = table["to"].str.split("\r", n=1, expand=True)
-        table[["tsegment", "toffset"]] = table["tsegment"].str.split("/", n=1, expand=True)
+def read_single_page_from_pdf(
+    filepath: Path | str,
+    page_number: int,
+    column_names: list = ["project_num", "SR_name", "from", "to", "scope", "miles", "muni", "adt"],
+) -> pd.DataFrame:
+    """
+    - Read a specific page of a PDF
+    - `tabula` returns a list containing dataframes. Use the first item
+       since we're reading one page at a time.
+    - Drop the first two rows of the resulting dataframe before returning
+    """
+    df = tabula.read_pdf(filepath, pages=page_number)[0]
+    df.columns = column_names
 
-        # split out rows with multiple segment/offsets into replacement and new rows
-        for k in range(0, len(table["from"])):
-            p = str(table.iloc[k, 0])  # project_num
-            s = str(table.iloc[k, 1])  # sr_name
-            f = str(table.iloc[k, 2])  # from
-            t = str(table.iloc[k, 3])  # to
-            sc = str(table.iloc[k, 4])  # scope
-            m = str(table.iloc[k, 5])  # miles
-            mu = str(table.iloc[k, 6])  # muni
-            a = str(table.iloc[k, 7])  # adt
-            fseg = str(table.iloc[k, 8])  # fsegment
-            foff = str(table.iloc[k, 9])  # foffset
-            tseg = str(table.iloc[k, 10])  # tsegment
-            toff = str(table.iloc[k, 11])  # toffset
+    # drop rows 1 & 2 and column 1 (with assigned row numbers)
+    # df = df.iloc[2:, :]
+    rows_to_drop = [0, 1]
+    df.drop(rows_to_drop, inplace=True)
 
-            if f[0].isdigit() == True:
-                # index = k
-                new_index = k + 1
-                [f1, f2] = f.split("\r", 1)
-                [fs, fo] = f1.split("/", 1)
-                [t1, t2] = t.split("\r", 1)
-                [ts, to] = t1.split("/", 1)
-                [m1, m2] = m.split("\r", 1)
+    return df
 
-                replacement_row = [p, s, f2, t2, sc, m1, mu, a, fseg, foff, tseg, toff]
-                new_row = [p, s, f2, t2, sc, m2, mu, a, fs, fo, ts, to]
 
-                for a in range(0, len(replacement_row)):
-                    table.loc[k, a] = replacement_row[a]
-                table = Insert_row(new_index, table, new_row)
+if __name__ == "__main__":
+    for filepath in Path(pdf_folderpath).rglob("*.pdf"):
 
-                print(table.iloc[k])
-                print(table.iloc[k + 1])
+        num_pgs = get_number_of_pages_in_pdf(filepath)
 
-        break
-    break
+        # append records from each page to dataframe
+        frames = []
+        for i in range(1, num_pgs + 1):
+            df = read_single_page_from_pdf(filepath, page_number=i)
 
-    # frames.append(table)
+            # parse out segment/offset from 'from' and 'to' columns for mapping
+            df[["fsegment", "from"]] = df["from"].str.split("\r", n=1, expand=True)
+            df[["fsegment", "foffset"]] = df["fsegment"].str.split("/", n=1, expand=True)
+            df[["tsegment", "to"]] = df["to"].str.split("\r", n=1, expand=True)
+            df[["tsegment", "toffset"]] = df["tsegment"].str.split("/", n=1, expand=True)
 
-    # allpgs = pd.concat(frames)
-    # allpgs.to_csv('D:/dvrpc_shared/BFR_Tracking/paving_package/CSVs/%s.csv' % filenames[j])
+            # split out rows with multiple segment/offsets into replacement and new rows
+            for k in range(0, len(df["from"])):
+                p = str(df.iloc[k, 0])  # project_num
+                s = str(df.iloc[k, 1])  # sr_name
+                f = str(df.iloc[k, 2])  # from
+                t = str(df.iloc[k, 3])  # to
+                sc = str(df.iloc[k, 4])  # scope
+                m = str(df.iloc[k, 5])  # miles
+                mu = str(df.iloc[k, 6])  # muni
+                a = str(df.iloc[k, 7])  # adt
+                fseg = str(df.iloc[k, 8])  # fsegment
+                foff = str(df.iloc[k, 9])  # foffset
+                tseg = str(df.iloc[k, 10])  # tsegment
+                toff = str(df.iloc[k, 11])  # toffset
+
+                if f[0].isdigit() == True:
+                    # index = k
+                    new_index = k + 1
+                    [f1, f2] = f.split("\r", 1)
+                    [fs, fo] = f1.split("/", 1)
+                    [t1, t2] = t.split("\r", 1)
+                    [ts, to] = t1.split("/", 1)
+                    [m1, m2] = m.split("\r", 1)
+
+                    replacement_row = [p, s, f2, t2, sc, m1, mu, a, fseg, foff, tseg, toff]
+                    new_row = [p, s, f2, t2, sc, m2, mu, a, fs, fo, ts, to]
+
+                    for a in range(0, len(replacement_row)):
+                        df.loc[k, a] = replacement_row[a]
+                    df = Insert_row(new_index, df, new_row)
+
+                    print(df.iloc[k])
+                    print(df.iloc[k + 1])
+
+        # frames.append(table)
+
+        # allpgs = pd.concat(frames)
+        # allpgs.to_csv('D:/dvrpc_shared/BFR_Tracking/paving_package/CSVs/%s.csv' % filenames[j])
