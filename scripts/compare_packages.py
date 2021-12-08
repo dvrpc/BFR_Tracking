18daf8c60984bded2c1db7668f3ff2eb42362a41
aaronfraint <aaronfraint@icloud.com>
2021-12-08 17:28:44 -0500

    add Sarah's latest commit

--- scripts/compare_packages.py
+++ scripts/compare_packages.py
@@ -1,9 +1,26 @@
 import psycopg2 as psql
-#import pandas as pd
+
+# import pandas as pd
 import geopandas as gpd
-#import os
+
+# import os
 from shapely import geometry, wkt
 from sqlalchemy import create_engine
 from geoalchemy2 import Geometry, WKTElement
 
-#compare package to screening records and/or projects on the shelf
+# compare package to screening records and/or projects on the shelf
+"""
+select 
+pt.*
+fo."CALENDAR_YEAR",
+fo."PLANNED"
+from package_test pt
+left join from_oracle  fo
+on cast(pt.sr AS numeric) = fo."STATE_ROUTE" 
+and cast(pt.fsegment as numeric) = fo."SEGMENT_FROM" 
+and cast(pt.tsegment as numeric) = fo."SEGMENT_TO"
+order by routenum;
+"""
+
+# add status field to oracle table that is manually updated from a drop down list
+# this status is reported along with the package once joined
