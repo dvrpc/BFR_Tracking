c71405503b16e935b5364b91acfdf1527dd4f445
aaronfraint <aaronfraint@icloud.com>
2021-12-09 09:26:23 -0500

    install codebase as a package into env

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="bfr_tracking",
+    packages=find_packages(),
+    version="0.0.2",
+    description="Bicycle-Friendly Resurfacing program tracking database",
+    author="Sarah Moran, AICP",
+    license="?",
+)
