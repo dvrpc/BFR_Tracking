52c14c693eb26a4782b4629868582e3049c948e5
aaronfraint <aaronfraint@icloud.com>
2021-12-09 09:10:39 -0500

    remove total row, clean up output

--- scripts/scrape_packages.py
+++ scripts/scrape_packages.py
@@ -1,13 +1,54 @@
-####
-# TO DO:
-# need a way to flag and fix multipiece segments
-# delete bottom row - totals
+"""
+scrape_packages.py
+------------------
+
+This script loops over every PDF within a given folder.
+For each PDF, it extracts tabular data from every page,
+parses attributes into new columns, and adds new rows
+for any PDF rows that contain two segments instead of one.
+"""
+
 from __future__ import annotations
 from pathlib import Path
 import tabula
-import os
 import pandas as pd
 from PyPDF2 import PdfFileReader
+
+
+def remove_returns_from_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function removes all occurances of '\r' in every column
+    of the input dataframe.
+    """
+    for column in df.columns:
+        df[column] = df[column].str.replace("\r", " ")
+    return df
+
+
+def drop_total_row_if_exists(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    The final page of the PDF set has a 'Total' row.
+    This function finds the index of that row if it exists,
+    and then drops that row.
+    """
+    for idx, row in df.iterrows():
+        if type(row["project_num"]) == str:
+            if row["project_num"].lower() == "total":
+                df.drop(idx, inplace=True)
+
+    return df
+
+
+def sort_df_by_project_id(df: pd.DataFrame, sort_column: str = "project_num") -> pd.DataFrame:
+    """
+    After adding new rows, we want to be able to sort the resulting table
+    before writing it to file.
+    """
+    df[sort_column] = df[sort_column].astype(float)
+
+    df.sort_values(sort_column, inplace=True)
+
+    return df
 
 
 def get_number_of_pages_in_pdf(filepath: Path | str) -> int:
@@ -16,7 +57,9 @@ def get_number_of_pages_in_pdf(filepath: Path | str) -> int:
     """
     with open(filepath, "rb") as pdf_file:
         pdf_reader = PdfFileReader(pdf_file)
-        return pdf_reader.numPages
+        number_of_pages = pdf_reader.numPages
+
+    return number_of_pages
 
 
 def read_single_page_from_pdf(
@@ -37,6 +80,9 @@ def read_single_page_from_pdf(
     # df = df.iloc[2:, :]
     rows_to_drop = [0, 1]
     df.drop(rows_to_drop, inplace=True)
+
+    # drop the 'total' row if it's in the dataframe
+    df = drop_total_row_if_exists(df)
 
     return df
 
@@ -73,14 +119,46 @@ def row_should_become_two(row: pd.Series) -> bool:
     return str(row["from"])[0].isdigit()
 
 
-def parse_single_pdf(filepath: Path) -> pd.DataFrame:
+def parse_single_page(filepath: Path, page_number: int) -> pd.DataFrame:
     """
-    Run all of the business logic for a single PDF file.
-
-    - Get the number of pages, and loop through each one
+    Extract data from a single page of a PDF
     - Identify rows that need to be split apart
     - For any of these rows, modify the existing row and add a new one
+    """
+    df = read_single_page_from_pdf(filepath, page_number=page_number)
 
+    df = extract_data_from_columns(df)
+
+    for idx, row in df.iterrows():
+        if row_should_become_two(row):
+            # extract values from specific cells in the row
+            f1, f2 = row["from"].split("\r", 1)
+            fs, fo = f1.split("/", 1)
+            t1, t2 = row["to"].split("\r", 1)
+            ts, to = t1.split("/", 1)
+            m1, m2 = row["miles"].split("\r", 1)
+
+            # update existing row
+            row["from"] = f2
+            row["to"] = t2
+            row["miles"] = m1
+
+            # add new row to dataframe
+            new_row = dict(row.copy())
+            new_row["miles"] = m2
+            new_row["fsegment"] = fs
+            new_row["foffset"] = fo
+            new_row["tsegment"] = ts
+            new_row["toffset"] = to
+
+            df = df.append(new_row, ignore_index=True)
+
+    return df
+
+
+def parse_single_pdf(filepath: Path, output_folder: Path) -> pd.DataFrame:
+    """
+    Run all of the business logic for a single PDF file which has multiple pages.
     - After repeating this for each page, merge all results into a singluar CSV output
     """
     print(f"Parsing: {filepath.stem}")
@@ -90,51 +168,25 @@ def parse_single_pdf(filepath: Path) -> pd.DataFrame:
     # append records from each page to dataframe
     frames = []
     for i in range(1, num_pgs + 1):
-        df = read_single_page_from_pdf(filepath, page_number=i)
 
-        df = extract_data_from_columns(df)
-
-        output_filepath = filepath.parent / f"{filepath.stem}-{i}.json"
-        df.to_json(output_filepath)
-
-        for idx, row in df.iterrows():
-            if row_should_become_two(row):
-                print("\t Splitting into two:")
-                print(row)
-
-                # extract values from specific cells in the row
-                f1, f2 = row["from"].split("\r", 1)
-                fs, fo = f1.split("/", 1)
-                t1, t2 = row["to"].split("\r", 1)
-                ts, to = t1.split("/", 1)
-                m1, m2 = row["miles"].split("\r", 1)
-
-                # update existing row
-                row["from"] = f2
-                row["to"] = t2
-                row["miles"] = m1
-
-                # add new row to dataframe
-                new_row = dict(row.copy())
-                new_row["miles"] = m2
-                new_row["fsegment"] = fs
-                new_row["foffset"] = fo
-                new_row["tsegment"] = ts
-                new_row["toffset"] = to
-
-                df = df.append(new_row, ignore_index=True)
+        df = parse_single_page(filepath, page_number=i)
 
         frames.append(df)
 
     allpgs = pd.concat(frames, ignore_index=True)
 
-    output_filepath = filepath.parent / f"{filepath.stem}.json"
+    cleaned_df = remove_returns_from_dataframe(allpgs)
+    cleaned_df = sort_df_by_project_id(cleaned_df)
 
-    allpgs.to_json(output_filepath)
+    output_filepath = output_folder / f"{filepath.stem}.csv"
+
+    cleaned_df.to_csv(output_filepath, index=False)
 
 
 if __name__ == "__main__":
-    pdf_folderpath = "./data/paving_package"
+    data_folder = Path("./data")
+    paving_package = data_folder / "paving_package"
+    output_folder = paving_package / "CSVs"
 
-    for filepath in Path(pdf_folderpath).rglob("*.pdf"):
-        parse_single_pdf(filepath)
+    for filepath in paving_package.rglob("*.pdf"):
+        parse_single_pdf(filepath, output_folder)
