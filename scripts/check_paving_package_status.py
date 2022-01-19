ffa5d5fc3fe5310dac698323dda6fa09b2c58ef4
smoran3 <smoran@dvrpc.org>
2022-01-19 11:58:43 -0500

    re-doing package revisions

--- scripts/check_paving_package_status.py
+++ scripts/check_paving_package_status.py
@@ -16,7 +16,8 @@ from make_folium_map import main as webmapper
 print("Importing from Oracle")
 importer()
 print("Scraping Paving Packages")
-scrape()
+# do not re-scrape packages that have been revised. Instead, use package_revision.py
+# scrape()
 print("Comparing and Mapping Paving Package Segments")
 mapper()
 print("Rendering Webmap")
