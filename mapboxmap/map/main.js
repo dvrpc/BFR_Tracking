d651f0d8f9872a63b8a108483da9744ad926a534
smoran3 <smoran@dvrpc.org>
2022-01-27 09:50:45 -0500

    added map popups

--- mapboxmap/map/main.js
+++ mapboxmap/map/main.js
@@ -29,14 +29,63 @@ map.on("load", () => {
   for (const source in sources) map.addSource(source, sources[source]);
   //load layer styles
   for (const layer in layers) map.addLayer(layers[layer]);
-});
 
-//popups
-map.on("mouseenter", "planned-segments", () => {
-  map.getCanvas().style.cursor = "pointer";
-});
-map.on("mouseleave", "planned-segments", () => {
-  map.getCanvas().style.cursor = "";
-});
+  //popups
+  map.on("mouseenter", "planned-segments", (e) => {
+    map.getCanvas().style.cursor = "pointer";
 
-//popup_on_click(map);
+    map.on("click", "planned-segments", (e) => {
+      let properties = e.features[0].properties;
+      let year = properties["Year"];
+      let name = properties["Road Name"];
+      let sr = properties["sr"];
+      let length = properties["Miles Planned"];
+
+      let message = `
+      <h4>${name} /SR ${sr}</h4>
+      <p>Planned Milage: ${length}</p>
+      <p>Planned Year: ${year}</p>
+    `;
+
+      let popup = new mapboxgl.Popup({
+        closeButton: false,
+        className: "popup-style",
+      });
+
+      popup.setLngLat(e.lngLat).setHTML(message).addTo(map);
+    });
+  });
+  map.on("mouseleave", "planned-segments", () => {
+    map.getCanvas().style.cursor = "";
+    let popup = document.getElementsByClassName("popup-style");
+
+    if (popup.length) {
+      popup[0].remove();
+    }
+  });
+  //B12
+  map.on("mouseenter", "B12", (e) => {
+    map.getCanvas().style.cursor = "pointer";
+
+    map.on("click", "B12", (e) => {
+      let message = `
+      <p>Paving Package: B12</p>
+    `;
+
+      let popup = new mapboxgl.Popup({
+        closeButton: false,
+        className: "popup-style",
+      });
+
+      popup.setLngLat(e.lngLat).setHTML(message).addTo(map);
+    });
+  });
+  map.on("mouseleave", "B12", () => {
+    map.getCanvas().style.cursor = "";
+    let popup = document.getElementsByClassName("popup-style");
+
+    if (popup.length) {
+      popup[0].remove();
+    }
+  });
+});
