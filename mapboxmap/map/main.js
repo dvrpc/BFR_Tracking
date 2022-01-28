2eacac8cd157b3c5dbcfd15516b59c20c72983af
smoran3 <smoran@dvrpc.org>
2022-01-28 14:24:10 -0500

    working on popups

--- mapboxmap/map/main.js
+++ mapboxmap/map/main.js
@@ -1,6 +1,5 @@
 import sources from "./mapSources.js";
 import layers from "./mapLayers.js";
-//import popup_on_click from "./popups.js";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZHZycGNvbWFkIiwiYSI6ImNrczZlNDBkZzFnOG0ydm50bXR0dTJ4cGYifQ.VaJDo9EtH2JyzKm3cC0ypA";
@@ -24,36 +23,51 @@ map.addControl(
 //add mabox standard navigation controls; zoom in/out and rotate
 map.addControl(new mapboxgl.NavigationControl());
 
+const popupmessage = (e) => {
+  var num = e.features.length;
+  var properties = features[0].properties;
+
+  let year = properties["Year"];
+  let name = properties["Road Name"];
+  let sr = properties["sr"];
+  let length = properties["Miles Planned"];
+  if (num <= 1)
+    return `
+    <h4>${name} /SR ${sr}</h4>
+    <p>Planned Milage: ${length}</p>
+    <p>Planned Year: ${year}</p>`;
+  else
+    return `
+    <h4>${name} /SR ${sr}</h4>
+    <p>Planned Milage: ${length}</p>
+    <p>Planned Year: ${year}</p>
+    <h4>Included in 2022 paving package</h4>
+    `;
+};
+
 map.on("load", () => {
   //load sources
   for (const source in sources) map.addSource(source, sources[source]);
   //load layer styles
   for (const layer in layers) map.addLayer(layers[layer]);
 
+  map.on("click", (e) => {
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: ["planned-segments", "B12", "C12", "D13", "M11", "P12"],
+    });
+    let message = popupmessage;
+
+    let popup = new mapboxgl.Popup({
+      closeButton: false,
+      className: "popup-style",
+    });
+
+    popup.setLngLat(e.lngLat).setHTML(message).addTo(map);
+  });
+
   //popups
   map.on("mouseenter", "planned-segments", (e) => {
     map.getCanvas().style.cursor = "pointer";
-
-    map.on("click", "planned-segments", (e) => {
-      let properties = e.features[0].properties;
-      let year = properties["Year"];
-      let name = properties["Road Name"];
-      let sr = properties["sr"];
-      let length = properties["Miles Planned"];
-
-      let message = `
-      <h4>${name} /SR ${sr}</h4>
-      <p>Planned Milage: ${length}</p>
-      <p>Planned Year: ${year}</p>
-    `;
-
-      let popup = new mapboxgl.Popup({
-        closeButton: false,
-        className: "popup-style",
-      });
-
-      popup.setLngLat(e.lngLat).setHTML(message).addTo(map);
-    });
   });
   map.on("mouseleave", "planned-segments", () => {
     map.getCanvas().style.cursor = "";
