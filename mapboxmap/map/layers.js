023976790deef947709901036b13ad3e185df676
Aaron Fraint <aaronfraint@icloud.com>
2022-01-29 13:47:52 -0500

    rename layers file

--- mapboxmap/map/layers.js
+++ mapboxmap/map/layers.js
@@ -1,11 +1,14 @@
+const yellow_paint_style = {
+  "line-width": 4,
+  "line-color": "yellow",
+  "line-opacity": 0.4,
+};
+
 const layers = {
   plan: {
     id: "planned-segments",
     type: "line",
     source: "plan",
-    layout: {
-      visibility: "visible",
-    },
     paint: {
       "line-width": 1.5,
       "line-color": [
@@ -29,55 +32,32 @@ const layers = {
     id: "B12",
     type: "line",
     source: "B12",
-    layout: {
-      visibility: "visible",
-    },
-    paint: {
-      "line-width": 4,
-      "line-color": "yellow",
-      "line-opacity": 0.4,
-    },
+    paint: yellow_paint_style,
   },
   C12: {
     id: "C12",
     type: "line",
     source: "C12",
-    paint: {
-      "line-width": 4,
-      "line-color": "yellow",
-      "line-opacity": 0.4,
-    },
+    paint: yellow_paint_style,
   },
   D13: {
     id: "D13",
     type: "line",
     source: "D13",
-    paint: {
-      "line-width": 4,
-      "line-color": "yellow",
-      "line-opacity": 0.4,
-    },
+    paint: yellow_paint_style,
   },
   M11: {
     id: "M11",
     type: "line",
     source: "M11",
-    paint: {
-      "line-width": 4,
-      "line-color": "yellow",
-      "line-opacity": 0.4,
-    },
+    paint: yellow_paint_style,
   },
   P12: {
     id: "P12",
     type: "line",
     source: "P12",
-    paint: {
-      "line-width": 4,
-      "line-color": "yellow",
-      "line-opacity": 0.4,
-    },
+    paint: yellow_paint_style,
   },
 };
 
-export default layers;
+export { layers };
