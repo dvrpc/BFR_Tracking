1d4b08bfe92bfe0c739d3d2dd9489e1bafa4b1f0
smoran3 <smoran@dvrpc.org>
2022-09-29 15:54:38 -0400

    adding more status update queries

--- scripts/import_filters.py
+++ scripts/import_filters.py
@@ -19,29 +19,51 @@ q_create_field = """ALTER TABLE filter_flags
 
 q_initial_screening = """UPDATE filter_flags
                         SET status = 'Initial Screening'
-                        WHERE "For Initial Screening"= 1; """
+                        WHERE " For Initial Screening"= 1; """
 
 q_dvrpc_screen = """UPDATE filter_flags
                     SET status = 'For DVRPC to Screen for Feasibility'
-                    WHERE "For DVRPC to Screen for Feasiblity"  = 1; """ 
+                    WHERE "	For DVRPC to Screen for Feasiblity"  = 1; """ 
+
+q_pending_review =  """UPDATE filter_flags
+                    SET status = 'Pending Review'
+                    WHERE "	Pending Review" = 1; """
 
 q_penndot_screen = """UPDATE filter_flags
-                    SET status = 'Pending Review'
-                    WHERE "Pending Review" = 1; """
+                    SET status = 'For PennDOT to Screen for Feasibility'
+                    WHERE " For PennDOT to Screen for Feasibility" = 1;"""
 
-q_outreach_prio = 
+q_outreach_prio = """UPDATE filter_flags
+                    SET status = 'Outreach Prioritization'
+                    WHERE " Outreach Prioritization" = 1; """
 
-q_begin_outreach = 
+q_begin_outreach = """UPDATE filter_flags
+                    SET status = 'Begin Municipal Outreach'
+                    WHERE "	To begin municipal outreach" = 1; """
 
-q_letter_pending = 
+q_letter_pending = """UPDATE filter_flags
+                    SET status = 'Waiting for municipal letter'
+                    WHERE "	Waiting for municipal letter"  = 1;"""
 
-q_letter_received = 
+q_letter_received = """UPDATE filter_flags
+                    SET status = 'Letter Received'
+                    WHERE "	Municipal letter received/Needs striping plan" = 1;"""
 
-q_completed = 
+q_completed = """UPDATE filter_flags
+                SET status = 'Complete'
+                WHERE "	Completed Projects" = 1; """
 
-q_capacity_analysis = 
+q_capacity_analysis = """UPDATE filter_flags
+                        SET status = 'Pending Capacity Analysis'
+                        WHERE " Pending Capacity Analysis" = 1; """
 
-q_beyond_scope =
+q_beyond_scope = """UPDATE filter_flags
+                    SET status = 'Beyond Scope of Resurfacing'
+                    WHERE "	Beyond scope of resurfacing" = 1; """
+
+q_no_status = """UPDATE filter_flags
+                SET status = 'Not Reviewed'
+                WHERE status = 'N/A'; """
 
 
 def main():
@@ -50,6 +72,8 @@ def main():
     df = pd.read_csv(filters)
     # write dataframe to postgis, replacing old table
     df.to_sql("filter_flags", con=ENGINE, if_exists="replace")
+
+    
 
 
 
